26cbf6a5f624d1ab58640437820fb26186b7ac4c
marklileo <phornphatmak1112@gmail.com>
2023-10-16 01:05:58 +0700

    Update Practice.js
    Update to IF ELSE lesson #75

--- Practice.js
+++ Practice.js
@@ -89,6 +89,298 @@ function fun2() {
   }
   console.log(outp);
 }
-
 fun1()
 fun2();
+/* ----------------------------------------------Local variable --> declare var, const, let variables inside the function-------------------------------------------- */
+function testLocalScope()
+{
+  var testLocal = "Test for Local variable";
+  console.log(testLocal);
+}
+testLocalScope();
+//console.log(testLocal); //Error!
+/* --------------------------------------------- Global vs. Local inside the funtion --> Declare both local and global vaiables with the same name--------------------- */
+const outerWear = "T-Shirt";
+function myOutfit() {
+  var outerWear = "sweater"
+  return outerWear;
+}
+myOutfit(); //The local variable will show
+/* --------------------------------------------- Global variable with the variable inside local--------------------- */
+let sum = 0; //Declare as global variable
+
+function addThree() {
+  sum = sum + 3;
+}
+function addFive()
+{
+  sum += 5;
+}
+
+addThree(); // sum update to 3
+addFive(); // sum = 3 + 5 = 8
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7); //Have variable to take the return value of a function 
+
+/* --------------------------------------------- ฺBoolean Valu --> True and False--------------------- */
+function welcomeToBooleans() {
+  return true; //Can return via function
+}
+welcomeToBooleans();
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+function trueOrFalse(wasThatTrue)
+{
+  if(wasThatTrue) //Or (wasThatTrue == true)
+  {
+    return "Yes, that was true";
+  }
+  else
+  {
+    return "No, that was false";
+  }
+}
+
+trueOrFalse(true);
+trueOrFalse(false);
+
+/* --------------------------------------------- Comparison with the Equality Operator ( == )--------------------- */
+
+/* NOTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  1   ==  1  // true
+  1   ==  2  // false
+  1   == '1' // true  "Enable type conversion"
+  "3" ==  3  // true  "Enable type conversion"
+*/
+// Setup
+function testEqual(val) {
+  if (val == 12) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+testEqual(10);
+testEqual(12);
+testEqual("12");
+/* --------------------------------------------- Comparison with the Strict Equality Operator --------------------- */
+
+/* NOTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+3 ===  3  // true
+3 === '3' // false "Not 'Enable' type conversion"
+*/
+function testStrict(val) {
+  if (val === 7) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+testStrict(10);
+testStrict(7);
+testStrict("7");
+
+/* --------------------------------------------- Comparison with the Inequality Operator--------------------- */
+/* NOTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+1 !=  2    // true
+1 != "1"   // false
+1 != '1'   // false
+1 != true  // false
+0 != false // false
+*/ 
+function testNotEqual(val) {
+  if (val != 99) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+testNotEqual(99); //Equal
+/* --------------------------------------------- Comparison with the Strict Inequality Operator --------------------- */
+/* ์NOTE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+3 !==  3  // false
+3 !== '3' // true
+4 !==  3  // true
+*/
+function testStrictNotEqual(val) {
+  if (val !== 17) { 
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(17); //Equal 
+/* --------------------------------------------- Comparison with the Greater Than Operator --------------------- */
+/* NOTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+5   >  3  // true
+7   > '3' // true  "Enable type conversion"
+2   >  3  // false
+'1' >  9  // false "Enable type conversion"
+*/
+
+function testGreaterThan(val) {
+  if (val > 100) {  
+    return "Over 100";
+  }
+
+  if (val > 10) {  
+    return "Over 10";
+  }
+  return "10 or Under";
+}
+
+testGreaterThan(10); // 10 or Under
+/* --------------------------------------------- Comparison with the Greater Than Or Equal To Operator--------------------- */
+/*
+6   >=  6  // true
+7   >= '3' // true "Enable type conversion"
+2   >=  3  // false
+'7' >=  9  // false "Enable type conversion"
+*/
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+testGreaterOrEqual(10); // 10 or Over
+
+/* --------------------------------------------- Comparisons with the Logical AND(&&) Operator--------------------- */
+function testLogicalAnd(val) {
+  if (val >= 25 && val <= 50) {
+    return "Yes";
+  }
+  return "No";
+}
+testLogicalAnd(10); // "No"
+
+/* --------------------------------------------- Comparisons with the Logical OR(||) Operator--------------------- */
+function testLogicalOr(val) {
+  if (val<10 || val > 20) {
+    return "Outside";
+  } else{
+  return "Inside";
+  }
+}
+
+testLogicalOr(15); //Inside
+testLogicalOr(25); //Outside
+
+/* --------------------------------------------- Logical Order in If Else Statements (Order is important)--------------------- */
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+orderMyLogic(4); // "Less than 5"
+orderMyLogic(7); //"Less than 10"
+
+/* --------------------------------------------- Chaining If Else Statements --------------------- */
+function testSize(num) {
+  if (num < 5)
+  {
+    return "Tiny";
+  }
+  else if(num < 10)
+  {
+    return "Small";
+  }
+  else if(num < 15)
+  {
+    return "Medium";
+  }
+  else if(num<20)
+  {
+    return "Large";
+  }
+  else
+  {
+    return "Huge"
+  }
+}
+testSize(7); //Small
+testSize(14); //Medium
+/* -------------------------------------------- Golf Code --------------------- */
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  if (strokes == 1)
+  {
+    return names[0];
+  }
+  else if( strokes <= par - 2 )
+  {
+    return names[1];
+  }
+  else if ( strokes == par - 1)
+  {
+    return names[2];
+  }
+  else if( strokes == par)
+  {
+    return names[3];
+  }
+  else if (strokes == par +1)
+  {
+    return names[4];
+  }
+  else if (strokes == par + 2)
+  {
+    return names[5];
+  }
+  else if( strokes >= par + 3)
+  {
+    return names[6];
+  }
+}
+golfScore(5, 4); // Birdie
+golfScore(4, 3); //Birdie
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
+
+
+/* --------------------------------------------- Recieve output of "return" from function--------------------- */
