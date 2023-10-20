00966fe516fc8b9ace244ba81f1b8c949188c3d2
marklileo <phornphatmak1112@gmail.com>
2023-10-21 01:52:32 +0700

    Update Practice.js
    Update 1 exercise

--- Practice.js
+++ Practice.js
@@ -651,15 +651,63 @@ const myPlants = [
       "pine",
       "birch"
     ]
+  },
+  {
+    type: "Test"
   }
 ];
 
 const secondTree = myPlants[1].list[1];  //Output: pine
-
+myPlants
 /* --------------------------------------------- JavaScript Objects--------------------------------------- */
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
 
+// Only change code below this line
+function updateRecords(records, id, prop, value)
+{
+  if(value === "")
+  {
+    delete records[id][prop];
+  }
+  else if(prop != "tracks")
+  {
+    records[id][prop] = value;
+  }
+  else if(prop == "tracks")
+  {
+    if(records[id].hasOwnProperty(prop)) //If the property exists inside object 
+    {
+      records[id][prop].push(value)
+    }
+    else
+    {
+      records[id][prop] = []; //assign as array
+      records[id][prop].push(value);
+    }
+  }
+  return records;
+}
 
-
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 /* --------------------------------------------- JavaScript Objects--------------------------------------- */
 
 
